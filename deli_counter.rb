b6693b9dbf4673406e8d3069fc2d3720bfd460c4
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-01-23 05:36:38 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 katz_deli = []
 
 def line(katz_deli)
-if there is nobody in line
+if katz_deli.length == 0
   puts "The line is currently empty."
+else katz_deli.length > 0 
+  puts 
+  
+
+  end 
 end 
+
