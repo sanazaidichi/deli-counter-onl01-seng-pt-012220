84dacd7ab944b8f6dd2153e9c60ce7dadd609096
github username <github email address>
2020-01-24 15:33:17 -0600

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,32 @@
+require 'pry'
+
 katz_deli = []
 
-def line(katz_deli)
-if katz_deli.length == 0
-  puts "The line is currently empty."
-else katz_deli.length > 0 
-  puts 
-  
-
+def line(arr)
+  if arr.length == 0 
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    counter = 1
+    arr.each do |name|
+      current_line += " #{counter}. #{name}"
+       counter += 1
+    end 
+    puts current_line
   end 
 end 
 
+def take_a_number(katz_deli, name)
+  katz_deli.push(name) 
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  end 
+
+def now_serving(katz_deli)
+  name = katz_deli[0]
+  if katz_deli.empty? 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{name}." 
+    katz_deli.shift
+  end 
+end 
