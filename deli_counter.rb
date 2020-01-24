d98fbdfd8af52fb913a3b663e5e2c5ef67cb2202
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-01-24 01:57:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 katz_deli = []
 
-def line(katz_deli)
-if katz_deli.length == 0
+def line(name)
+if name.length == 0
   puts "The line is currently empty."
-else katz_deli.length > 0 
-  puts 
-  
+else 
+  name.length +=0  
+    
 
   end 
 end 
